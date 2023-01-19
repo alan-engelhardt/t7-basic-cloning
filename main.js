259be56ec20db098b4bcd2459e8fc7eab1ff8ae6
Alan <ale@kea.dk>
2023-01-19 10:46:18 +0100

    oversat

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-//This is the object that you should use to "populate" the template
+//Dette er det objekt du skal bruge til at "populate" skabelonen (template)
+
 const gitHop = {
   name: "GitHop",
   imgSrc: "githop.png",
   description: "FooBar's Venom Ale",
 };
 
-//Step 1: Chose the <template>'s content
+//Step 1: Vælg (select) <template> content fra HTML og gem den i en const
 
-//Step 2: Make a "clone"
+//Step 2: Lav en "clone" af templatens content
 
-//Step 3: Change the content of the clone
-//use the data from the object above (you can do this when the rest is working)
+//Step 3: Ændr indholdet i clon'en til data i object'et ovenover
 
-//Step 4: Chose the new "parent" element
-//Append it to main
+//Step 4: Vælg <main> fra HTML og gem den i en const
 
-//Step 5: Add (Append) the clone to the DOM
+//Step 5: Tilføj (Append) den modificerede clon til DOM
